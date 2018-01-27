e8695669f6c86d5292378b3eb2a453a69c1f743c
Patricio Marrone <marrone.patricio@gmail.com>
2018-01-27 02:32:20 -0300

    Adding rudimentary component system

--- app/app.js
+++ app/app.js
@@ -1,22 +1,62 @@
 var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path
-var bunny = PIXI.Sprite.fromImage('/assets/images/node.png')
+const Textures = {
+  node: PIXI.Texture.fromImage('/assets/images/node.png')
+}
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+class Node {
+  constructor(x, y) {
+    this.sprite = new PIXI.Sprite(Textures.node)
+    this.sprite.x = x
+    this.sprite.y = y
+    this.sprite.anchor.set(0.5)
+  }
 
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+  update(delta) {
+    console.log(delta)
+    this.sprite.rotation += 0.1 * delta
+  }
+}
 
-app.stage.addChild(bunny);
+class Game {
+  constructor() {
+    this.components = []
+    app.ticker.add(delta => {
+      this.update(delta)
+    });
+  }
+  addComponent(component) {
+    if (component.sprite) {
+      app.stage.addChild(component.sprite)
+    }
+    this.components.push(component)
+  }
+
+  update(delta) {
+    this.components.forEach(component => {
+      if (component.update) {
+        component.update(delta)
+      }
+    })
+  }
+}
+
+const level1 = {
+  nodes: {
+    a: new Node(200, 500),
+    b: new Node(400, 400),
+    c: new Node(200, 200)
+  }
+}
+
+const game = new Game()
+
+for (let node in level1.nodes) {
+  game.addComponent(level1.nodes[node])
+}
+
+
 
 // Listen for animate update
-app.ticker.add(function(delta) {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent tranformation
-    bunny.rotation += 0.1 * delta;
-});
+
